b14df15571b139f50e9676dd93a68375cf1ec27f
Graham <delafield@wisc.edu>
2022-12-10 18:26:04 -0600

    finish day 7

--- day_7.py
+++ day_7.py
@@ -1,18 +1,8 @@
 from common_functions import read_data
-import re
-import json
-
-def pretty(d, indent=0):
-   for key, value in d.items():
-      print('\t' * indent + str(key))
-      if isinstance(value, dict):
-         pretty(value, indent+1)
-      else:
-         print('\t' * (indent+1) + str(value))
 
 def make_tree(data):
-    records = {}
     data = [d.replace('$ ', '') for d in data]
+    records = {}
     tree = []
     for d in data:
         console = d.split(' ')
@@ -20,57 +10,52 @@ def make_tree(data):
             case 'cd':
                 head_dir = console[1]
                 if head_dir == '..':
-                    head_dir = tree.pop(-1)
-                    continue
+                    this_dir = '/'.join(tree)
+                    total_to_add = records.get(this_dir, 0)
+
+                    tree = tree[:-1]
+                    this_dir = '/'.join(tree)
+                    records[this_dir] = records.get(this_dir, 0) + total_to_add
 
                 else:
                     tree.append(head_dir)
-                print(tree)
-
-                new = {head_dir:{'files':[],'dirs':[]}}
-                current = records.get(head_dir, new)
             
+                this_dir = '/'.join(tree)
+
             case 'ls':
                 pass 
-            
+                
             case 'dir':
-                print(current, head_dir)
-                current[head_dir]['dirs'].append(console[1])
-            
+                pass 
+                
             case other:
-                current[head_dir]['files'].append(int(console[0]))
+                records[this_dir] = records.get(this_dir, 0) + int(console[0])
+
+    while tree != ['/']:
+        this_dir = '/'.join(tree)
+        total_to_add = records.get(this_dir, 0)
         
-        records.update(current)
-    
+        tree = tree[:-1]
+        this_dir = '/'.join(tree)
+        records[this_dir] = records.get(this_dir, 0) + total_to_add
+
     return records
 
-def get_sum(tree, dir):
-    these_contents = tree[dir]
-
-    # get immediate contents
-    this_val = sum(these_contents['files'])
-
-    # recurse to get subcontents
-    sub_vals = sum([get_sum(tree, d) for d in these_contents['dirs']]) 
-    
-    return  this_val + sub_vals
-
-
-def get_totals(tree, max_size=100_000):
-    totals = []
-    for head, rec in tree.items():
-        print(f'{head} contains {rec["dirs"]}')
-        val = sum(rec['files']) + sum([get_sum(tree, d) for d in rec['dirs']])
-        print(head, val)
-        if val <= max_size:
-            totals.append(val)
-    print(sum(totals))
         
-    
+def get_sum_valid(tree):
+    ans = sum([v for v in tree.values() if v <= 100_000])
+    print(ans)
+
+def find_smallest_valid(tree):
+    total = 70000000
+    used = tree['/']
+
+    needed = 30000000 - (total - used) 
+    ans = min([v for v in tree.values() if v >= needed])
+    print(ans)
 
 if __name__ == '__main__':
     data = read_data('./data/day_7.txt')
     tree = make_tree(data)
-    print(tree)
-    get_totals(tree)
-
+    s = get_sum_valid(tree)
+    m = find_smallest_valid(tree)
