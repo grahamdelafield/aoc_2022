0579f013bdd8569bea72ad1be158c88e3c8ad492
Graham Delafield <delafield@wisc.edu>
2022-12-15 11:00:27 -0600

    update

--- day_12.py
+++ day_12.py
@@ -3,15 +3,16 @@ import numpy as np
 lets = [chr(i) for i in range(97, 123)]
 nums = [i for i in range(1, 27)]
 lets.extend(["S", "E"])
-nums.extend(["1", "26"])
+nums.extend([0, 27])
 SURVEY = dict(zip(lets, nums))
 
-def round1():
-    pass 
+def round1(land):
+    scape = np.vectorize(SURVEY.get)(land)
+    print(scape)
 
 if __name__ == '__main__':
     data = open('./data/day_12_test.txt', 'r').read()
     data = [[let for let in line] for line in data.split('\n')]
     land = np.array(data, dtype='str')
     
-    round1()
+    round1(land)
